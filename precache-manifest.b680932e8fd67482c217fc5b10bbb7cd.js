3709fbefcaa315823a9f50e91ec9136fdf97b067
aholachek <aholachek@gmail.com>
2019-08-06 10:27:43 -0400

    Updates

---
+++ precache-manifest.b680932e8fd67482c217fc5b10bbb7cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd1b7564d8b5525e5c183a8eab2da548",
+    "url": "/react-stripe-menu/index.html"
+  },
+  {
+    "revision": "f31748a19954cdc282df",
+    "url": "/react-stripe-menu/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "e28f0f3321c4a9f80934",
+    "url": "/react-stripe-menu/static/css/main.26c5cb05.chunk.css"
+  },
+  {
+    "revision": "f31748a19954cdc282df",
+    "url": "/react-stripe-menu/static/js/2.32364aed.chunk.js"
+  },
+  {
+    "revision": "e28f0f3321c4a9f80934",
+    "url": "/react-stripe-menu/static/js/main.998a85f3.chunk.js"
+  },
+  {
+    "revision": "7100eb6cf05c21472125",
+    "url": "/react-stripe-menu/static/js/runtime~main.cf391686.js"
+  }
+]);
